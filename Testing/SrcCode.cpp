e14d3bfbe9b15053803af16f2eb9e748300ae9c6
maestro194 <dongboy1904@gmail.com>
2023-04-15 14:44:59 +0700

    Update test

--- Testing/SrcCode.cpp
+++ Testing/SrcCode.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
+#include <chrono>
 #pragma GCC optimization ("unrolled-loops")
+
 using namespace std;
+using namespace std::chrono;
 
 #define filename "TestCase"
 #define endl '\n'
-#define X first
-#define Y second
 #define ll long long
-#define all(x) (x).begin(),(x).end()
 
 mt19937 mt(time(nullptr));
 mt19937_64 mt64(time(nullptr));
@@ -137,8 +137,16 @@ int32_t main()
   FileInOut();
   preSolve();
 
+  auto start_time = high_resolution_clock::now();
+
   int test = 1;
   // cin>>test;
   while(test--)
     Solve();
+
+  auto end_time = high_resolution_clock::now();
+  auto duration = duration_cast<nanoseconds>(end_time - start_time);
+
+  // cout << endl;
+  // cout << "Execution time: " << duration.count() << endl;
 }
--- Testing/TestCase.cpp
+++ Testing/TestCase.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
+#include <chrono>
 #pragma GCC optimization ("unrolled-loops")
+
 using namespace std;
+using namespace std::chrono;
 
 #define filename "TestCase"
 #define endl '\n'
-#define X first
-#define Y second
 #define ll long long
-#define all(x) (x).begin(),(x).end()
 
 mt19937 mt(time(nullptr));
 mt19937_64 mt64(time(nullptr));
@@ -15,32 +15,49 @@ mt19937_64 mt64(time(nullptr));
 void FileInOut()
 {
   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  // ifstream *fi = new ifstream (filename".inp");
   ofstream *fo = new ofstream (filename".inp");
-  if(*fo) 
+  if(*fo)
     cout.rdbuf( fo->rdbuf() );
-
 }
-//*******************************************************
+//*************************************************************
 
 
 
-void Solve()
-{
-  int n;
+void Solve() {
+  int n, q;
+
   n = 100000;
-  cout << n << endl;
-  for(int i = 0; i < n; i ++)
-    cout << 999000000 + mt() % 1000001 << " ";
-  
+  q = 100000;
+
+  cout << n << " " << q << endl;
+  for(int i = 0; i < n; i ++) {
+    cout << mt() % 1000000000 << " ";
+  }
+  cout << endl;
+
+  for(int i = 0; i < q; i ++) {
+    int u = 1 + mt() % n;
+    int v = 1 + mt() % n;
+    while(u == v)
+      v = 1 + mt() % n;
+    cout << u << " " << v << endl;
+  }
 }
 
 int32_t main()
 {
   FileInOut();
 
+  auto start_time = high_resolution_clock::now();
+
   int test = 1;
   // cin>>test;
   while(test--)
     Solve();
+
+  auto end_time = high_resolution_clock::now();
+  auto duration = duration_cast<nanoseconds>(end_time - start_time);
+
+  // cout << endl;
+  // cout << "Execution time: " << duration.count() << endl;
 }
